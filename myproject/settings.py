c0a4c84bc98c2d0b452d63f7093eb92c19fe5f65
darshik-chovatiya <drshikpatel21@gmail.com>
2025-05-14 15:55:02 +0530

    Update runtime.txt for Python 3.12

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
